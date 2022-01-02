c18732c52f487060795fe373ce57751e43f562c9
kumap75 <pramod_km@hotmail.com>
2022-01-02 13:38:52 +0000

    First draft

---
+++ dev1.py
@@ -0,0 +1,21 @@
+"""
+is_valid_input = False
+while is_valid_input is False:
+    try:
+        a = int(input ("please enter value of a: "))
+        print("value of a is: ",a)
+        is_valid_input = True
+    except:
+        print("Only integer values are allowed")
+        continue
+"""
+class cs:
+    def __init__(self,a,b):
+        self.a = a 
+        self.b = b
+    def mysum(self):
+        print("sum of the numbers is:",self.a + self.b)
+
+c = cs(1,2)
+c.mysum()
+
